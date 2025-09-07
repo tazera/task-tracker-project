687dd0847f5817ed7c3bd8737ec17bf6a54fbe31
Lyubomir <43777483+tazera@users.noreply.github.com>
2025-09-07 21:27:54 +0300

    Initialize the project with Maven setup and Spring Boot configuration

---
+++ src/test/java/com/pulpudev/tasktracker/TaskTrackerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulpudev.tasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
